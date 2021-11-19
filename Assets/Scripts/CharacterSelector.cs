627832bc22ebbae4ab9e6790f36a0b59d4a33170
lcscout <lcscoutinho@outlook.com>
2021-11-19 12:30:00 -0300

    a lot of shit

--- Assets/Scripts/CharacterSelector.cs
+++ Assets/Scripts/CharacterSelector.cs
@@ -1,15 +1,17 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using Mirror;
+using TMPro;
 
 public class CharacterSelector : MonoBehaviour {
 	//public GameObject player;
 
 	public GameObject[] characters;
 	public GameObject characterSelectPanel;
+	public TMP_Text choosenChar;
 
 	public void ChooseCharacter(int characterChoice) {
 		GameManager.Instance.choosenCharacter = characters[characterChoice];
+		choosenChar.text = characters[characterChoice].name;
 	}
 }
