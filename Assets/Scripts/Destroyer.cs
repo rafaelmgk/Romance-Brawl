01c20eb0b8e85bd7aff0f04778e3631c459be0e7
rafaelmgk <faelmagesky@gmail.com>
2021-11-21 13:00:18 -0300

    Add Destroyer

---
+++ Assets/Scripts/Destroyer.cs
@@ -0,0 +1,12 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Destroyer : MonoBehaviour
+{
+    // Start is called before the first frame update
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        Destroy(other.gameObject);
+    }
+}
