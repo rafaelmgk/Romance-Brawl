e88e00c2226280ec0e36bd64cf02a1fe95d6cf71
lcscout <lcscoutinho@outlook.com>
2021-11-21 13:36:22 -0300

    Add player respawn

--- Assets/Scripts/Destroyer.cs
+++ Assets/Scripts/Destroyer.cs
@@ -1,12 +1,18 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using Mirror;
 
-public class Destroyer : MonoBehaviour
-{
-    // Start is called before the first frame update
-    private void OnTriggerEnter2D(Collider2D other)
-    {
-        Destroy(other.gameObject);
-    }
+public class Destroyer : NetworkBehaviour {
+	private void OnTriggerEnter2D(Collider2D other) {
+		other.gameObject.SetActive(false);
+		StartCoroutine(WaitAndRespawn(other.gameObject));
+	}
+
+	private IEnumerator WaitAndRespawn(GameObject other) {
+		other.transform.position = new Vector3(0, 2, 0);
+
+		yield return new WaitForSeconds(1f);
+		other.SetActive(true);
+	}
 }
